208f9f6e801cb27830da7330be83ddb0423f5697
Wispy <101812473+WispySparks@users.noreply.github.com>
2024-09-06 17:40:24 -0500

    Fix invalid html/css/js, format code, small bug fixes (#40)

--- source/assets/javascripts/custom.js
+++ source/assets/javascripts/custom.js
@@ -1,22 +1,24 @@
-
-$('body').scrollspy({
-  'target': '#nav',
-  'offset': 40
+$("body").scrollspy({
+  target: "#nav",
+  offset: 40,
 });
 
-$('.carousel').each(function() {
+$(".carousel").each(function () {
   var speed = 5000;
-  if ($(this).attr('rel-speed') != null) {
-  	speed = $(this).attr('rel-speed');
+  if ($(this).data("rel-speed") != null) {
+    speed = $(this).data("rel-speed");
   }
   $(this).carousel({
-  	interval: speed
+    interval: speed,
   });
 });
 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
+$("a").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    500,
+  );
+  return false;
 });
