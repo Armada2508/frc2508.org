0ed2d572aa9b6f89551a6102a4db7881024cd35f
Wispy <101812473+WispySparks@users.noreply.github.com>
2024-10-11 16:03:31 -0500

    Remove unused assets (#59)

---
+++ source/assets/javascripts/main.js
@@ -0,0 +1,24 @@
+$("body").scrollspy({
+  target: "#nav",
+  offset: 40,
+});
+
+$(".carousel").each(function () {
+  var speed = 5000;
+  if ($(this).data("rel-speed") != null) {
+    speed = $(this).data("rel-speed");
+  }
+  $(this).carousel({
+    interval: speed,
+  });
+});
+
+$("a").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top,
+    },
+    500,
+  );
+  return false;
+});
